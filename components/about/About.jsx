0db92366a3ad8c2991e086140346591f40c89011
shahids6 <shahid@sideways6.in>
2025-12-22 15:50:37 +0530

    Update About.jsx

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -31,7 +31,8 @@ const About = () => {
           <div className={styles.textContent}>
             <h2 className="title1" ref={titleRef}><span>About Us</span></h2>
             <p className={styles.aboutText1} ref={subtitleRef} role="text">
-              <span>Sideways 6, a proud unit of AD6 Advertising, is a vibrant
+              <span>Sideways 6, a proud unit of AD6 Advertising, an event
+              management company is a vibrant
               collective of diverse yet like-minded individuals driven by a
               shared passion for innovation and excellence. Our guiding
               principles—'Delivering Unique Service' and 'Great Ideas'—form the
@@ -39,21 +40,13 @@ const About = () => {
               enthusiasm and a smile.</span>
             </p>
             <p className={styles.aboutText2} role="text">
-              <span>At Sideways 6, building brands we love isn't just our mission—it's
-              our art. We thrive on pushing boundaries, thinking differently,
-              and crafting experiences that resonate deeply with our clients'
-              brand values and integrity. From concept to execution, we approach
-              event planning with unparalleled creativity and precision,
-              ensuring each project is as unique and memorable as the story
-              behind it.</span>
+              <span>At Sideways 6, building brands we love isn't just our
+              mission—it's our art. We approach event planning with
+              unparalleled creativity and precision, ensuring each project
+              is as unique and memorable as the story behind it.</span>
             </p>
             {/* <p className={styles.aboutText3}>
-              <span>With a wealth of expertise, a commitment to excellence, and a
-              track record of success, we are equipped to design and execute
-              innovative, large-scale events that leave lasting impressions.
-              Year after year, we bring fresh ideas to life, creating
-              extraordinary moments that captivate audiences and strengthen
-              brand identities.</span>
+              <span>We bring creativity and precision to event management, delivering impactful corporate events that reflect true brand identity. As a leading event management company and trusted event planners, we handle everything from event decoration to creating the perfect stage for event experiences. Recognised among the top corporate event planners in Bangalore, our events company in Bangalore offers seamless events and services, making us the go-to corporate event management company in Bangalore.</span>
             </p> */}
           </div>
           <div className={styles.iframeContainer} role="complementary" aria-label="About us video">
